0f90b0a743d6cb2fe93bca10c893079668f22c7e
Amaan Faraaz <amaan2201@gmail.com>
2021-01-26 13:30:45 +0530

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,144 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+from typing import Any, Text, Dict, List
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from rasa_sdk import Action, Tracker
+from typing import Any, Text, Dict, List
+from rasa_sdk.executor import CollectingDispatcher
+from twilio.rest import Client
+from weather import Weather
+#Weather
+class ActionHelloWorld(Action):
+	def name(self) -> Text:
+		return "action_weather_api"
+
+	def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+		city=tracker.latest_message['text']
+		temp=int(Weather(city)['temp']-273)
+		dispatcher.utter_template("utter_temp",tracker,temp=temp)
+		return []
+
+
+#Email actions
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+
+        SendEmail(
+            tracker.get_slot("email"),
+            tracker.get_slot("subject"),
+            tracker.get_slot("message")
+        )
+        dispatcher.utter_message("Thanks for providing the details. We have sent you a mail at {}".format(tracker.get_slot("email")))
+        return []
+
+def SendEmail(toaddr,subject,message):
+    fromaddr = "amaan2201@gmail.com"
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
+
+    # storing the senders email address
+    msg['From'] = fromaddr
+
+    # storing the receivers email address
+    msg['To'] = toaddr
+
+    # storing the subject
+    msg['Subject'] = subject
+
+    # string to store the body of the mail
+    body = message
+
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+
+    # open the file to be sent
+    # filename = "/home/ashish/Downloads/webinar_rasa2_0.png"
+    # attachment = open(filename, "rb")
+    #
+    # # instance of MIMEBase and named as p
+    # p = MIMEBase('application', 'octet-stream')
+    #
+    # # To change the payload into encoded form
+    # p.set_payload((attachment).read())
+    #
+    # # encode into base64
+    # encoders.encode_base64(p)
+    #
+    # p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    #
+    # # attach the instance 'p' to instance 'msg'
+    # msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+
+    # Authentication
+    try:
+        s.login(fromaddr, "tensa zangetsu")
+
+        # Converts the Multipart msg into a string
+        text = msg.as_string()
+
+        # sending the mail
+        s.sendmail(fromaddr, toaddr, text)
+    except:
+        print("An Error occured while sending email.")
+    finally:
+        # terminating the session
+        s.quit()
+
+
+
+#SMS Actions
+
+class ActionSubmit2(Action):
+	def name(self) -> Text:
+		return "action_sms_submit"
+	def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+		account_sid = 'ACa8b10d2af3967a2ca723f768049ee7c9' 
+		auth_token = '6024cec75beb9dbd46fb64990a205d8e'
+		client = Client(account_sid, auth_token)
+		message = client.messages.create(from_='+12103901541',body=tracker.get_slot("sms_message"),to = tracker.get_slot("mobile_number"))
+		print(message.sid)
+		dispatcher.utter_message(text="Message has been sent successfully to {}".format(tracker.get_slot("mobile_number")))
+		return []
